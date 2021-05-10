1ef12f93d4f901a869c4c6664303fb92179072e2
zerontec <zerontecnologia1@gmail.com>
2021-05-09 21:57:27 -0400

    Instalacion Angular material

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home/home.component';
+import {MaterialModule} from '../material/material.module';
+
+
+@NgModule({
+  declarations: [HomeComponent],
+  imports: [
+    CommonModule,
+    MaterialModule
+  ],
+  exports:[
+    HomeComponent
+
+  ]
+})
+export class CoreModule { }
