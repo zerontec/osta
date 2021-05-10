3500e874adf6447f587b6df3dbfd9e25bac33d61
zerontec <zerontecnologia1@gmail.com>
2021-05-10 12:09:51 -0400

    Agragre Ruta Home Component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: `home`,
+   /*  data: { titulo: 'Home', preload: true }, */
+    loadChildren: () =>
+      import('./core/home/home.module').then((m) => m.HomeModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
